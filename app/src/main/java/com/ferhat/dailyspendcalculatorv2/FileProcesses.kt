420a2bf1adb83e8b30e401cfaca7dabffd3e19ac
Ferhat Sanli <ferhatsanli.nl@gmail.com>
2025-07-09 13:56:21 +0200

    Init

---
+++ app/src/main/java/com/ferhat/dailyspendcalculatorv2/FileProcesses.kt
@@ -0,0 +1,23 @@
+package com.ferhat.dailyspendcalculatorv2
+
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+
+object FileProcesses {
+    fun readCsv(context: Context, uri: Uri): List<List<String>> {
+        val res = mutableListOf<List<String>>()
+        context.contentResolver.openInputStream(uri)?.use {
+            inputStream ->
+                inputStream.bufferedReader().useLines {
+                    lines ->
+                        lines.forEach {
+                            line ->
+                                val columns = line.split(",")
+                                res.add(columns)
+                        }
+                }
+        }
+        return res
+    }
+}
---
+++ app/src/main/java/com/ferhat/dailyspendcalculatorv2/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.ferhat.dailyspendcalculatorv2
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ferhat.dailyspendcalculatorv2.ui.theme.DailySpendCalculatorV2Theme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        if (intent?.action == Intent.ACTION_SEND && intent.type?.contains("csv") == true) {
+            val csvUri = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
+            // readCsv
+        }
+
+        setContent {
+            DailySpendCalculatorV2Theme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DailySpendCalculatorV2Theme {
+        Greeting("Android")
+    }
+}
